7aec96215cdcbc8b5e7b622eb71ce237f16fbbe1
Adnan Alfadhli <adnan2000611@gmail.com>
2020-12-25 23:23:18 +0300

    Activated the number and delete tap

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Calculator: View {
     // MARK: - Add states here
-    
+    @State var number = ""
     var body: some View {
         ZStack{
             Color.black.edgesIgnoringSafeArea(.all)
@@ -17,37 +17,61 @@ struct Calculator: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Text("0")
+                    Text(number)
                         .modifier(TitleModifier())
                         .animation(.easeIn(duration: 0.1))
                 }
                 HStack{
-                    Text("C").modifier(ButtonModifier(type: .gray))
+                    Text("C").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number = ""
+                    }
                     Text("±").modifier(ButtonModifier(type: .gray))
                     Text("%").modifier(ButtonModifier(type: .gray))
                     Text("÷").modifier(ButtonModifier(type: .orange))
                 }
                 HStack{
-                    Text("7").modifier(ButtonModifier(type: .gray))
-                    Text("8").modifier(ButtonModifier(type: .gray))
-                    Text("9").modifier(ButtonModifier(type: .gray))
+                    Text("7").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "7"
+                    }
+                    Text("8").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "8"
+                    }
+                    Text("9").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "9"
+                    }
                     Text("×").modifier(ButtonModifier(type: .orange))
                 }
                 HStack{
-                    Text("4").modifier(ButtonModifier(type: .gray))
-                    Text("5").modifier(ButtonModifier(type: .gray))
-                    Text("6").modifier(ButtonModifier(type: .gray))
+                    Text("4").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "4"
+                    }
+                    Text("5").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "5"
+                    }
+                    Text("6").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "6"
+                    }
                     Text("-").modifier(ButtonModifier(type: .orange))
                 }
                 HStack{
-                    Text("1").modifier(ButtonModifier(type: .gray))
-                    Text("2").modifier(ButtonModifier(type: .gray))
-                    Text("3").modifier(ButtonModifier(type: .gray))
+                    Text("1").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "1"
+                    }
+                    Text("2").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "2"
+                    }
+                    Text("3").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "3"
+                    }
                     Text("+").modifier(ButtonModifier(type: .orange))
                 }
                 HStack{
-                    Text("0").modifier(Zero())
-                    Text(".").modifier(ButtonModifier(type: .gray))
+                    Text("0").modifier(Zero()).onTapGesture {
+                        number += "0"
+                    }
+                    Text(".").modifier(ButtonModifier(type: .gray)).onTapGesture {
+                        number += "."
+                    }
                     Text("=").modifier(ButtonModifier(type: .orange))
                 }
             }
